0911c090ad6a9792136b9818882290e86f03ebff
blaugranaa <aysenur.fcb@gmail.com>
2021-02-10 15:08:44 +0300

    Ball Random Color Change

---
+++ Assets/Scripts/BallColor.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallColor : MonoBehaviour
+{
+    [SerializeField]
+    Color purple, green, pink, blue;
+
+    MeshRenderer meshRenderer;
+
+    int index;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+    void Start()
+    {
+        index = Random.Range(0,4);
+        ColorObj();
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+    public void ColorObj()
+    {
+        switch (index)
+        {
+            case 0:
+                {
+                    meshRenderer.material.color = purple;
+                    break;
+                }
+            case 1:
+                {
+                    meshRenderer.material.color = blue;
+                    break;
+                }
+            case 2:
+                {
+                    meshRenderer.material.color = pink;
+                    break;
+                }
+            case 3:
+                {
+                    meshRenderer.material.color = green;
+                    break;
+                }
+
+            default:
+                break;
+        }
+    }
+
+}
