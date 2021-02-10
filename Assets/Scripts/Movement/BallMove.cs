5afad501258bf6ad74dd8c3ee77cfcb15d7f2df8
blaugranaa <aysenur.fcb@gmail.com>
2021-02-10 14:48:07 +0300

    Ball movement and tracker added

---
+++ Assets/Scripts/Movement/BallMove.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        MoveForward();
+    }
+
+    private void MoveForward()
+    {
+        if (rigidbody.velocity.magnitude <3.5f)
+        {
+            rigidbody.AddForce(Vector3.forward * speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Movement/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player = default;
+    private Vector3 _offset;
+
+    public void Start()
+    {
+        _offset = transform.position - player.transform.position;
+    }
+
+    public void Update()
+    {
+        transform.position = player.transform.position + _offset;
+    }
+}
